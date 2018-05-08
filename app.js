604bfe3e2eb82d11a680b629303a9bd4189c983b
Yury Hantsouski <Yury@Hantsouski.epam.com>
2018-05-08 10:22:56 +0300

    Neat the dirwatcher event handler in the app.js
    * Use async/await in the dirwatcher:changed async handler

--- app.js
+++ app.js
@@ -11,21 +11,19 @@ const importer = new Importer();
 
 const delay = config.watcherDelay;
 
-dirWatcher.on('dirwatcher:changed', path => {
-  importer
-    .import(path)
-    .then(data => {
-      console.log(`${path} has been changed`);
-      return data;
-    })
-    .then(console.log)
-    .catch(console.err);
-});
-dirWatcher.on('dirwatcher:changed', path => {
+dirWatcher.on('dirwatcher:changed', async path => {
+  const json = await importer.import(path);
   console.log(`${path} has been changed`);
+  console.log(json);
+});
+
+dirWatcher.on('dirwatcher:changed', path => {
+  console.log(`Sync ${path} has been changed`);
   console.log(importer.importSync(path));
 });
+
 dirWatcher.on('dirwatcher:deleted', path => {
   console.log(`${path} has been deleted`);
 });
+
 dirWatcher.watch(path.join(__dirname, '/data'), delay);
