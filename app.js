68e1cf7089c464f63ec07cbd1fc10e794f12d34f
Yury Hantsouski <Yury@Hantsouski.epam.com>
2018-07-23 17:12:19 +0300

    Implement the homework №8
    * Add the yaml file with all API

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 import routes from './server/routes';
+
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api', routes);
 
 export default app;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,11 @@
 import express from 'express';
 import products from './products';
 import users from './users';
-import cities from './cities';
 
 const routes = express.Router();
 
 routes.use('/products', products);
 routes.use('/users', users);
-routes.use('/cities', cities);
 
 routes.get('/', (req, res) => {
   res.status(200).json({ message: 'Connected' });
